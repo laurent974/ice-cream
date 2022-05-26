f4f053e31c82ef447d0313be1aa0a63cb34c5d5b
Laurent <lyenpon974@gmail.com>
2022-05-26 15:28:26 +0200

    Update - Workflow Front end

--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -1,24 +1,19 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix')
+require( 'laravel-mix-stylelint' )
+require('laravel-mix-eslint')
 
-require('dotenv').config();
-
-/*
-  |--------------------------------------------------------------------------
-  | Mix Asset Management
-  |--------------------------------------------------------------------------
-  |
-  | Mix provides a clean, fluent API for defining some Webpack build steps
-  | for your WordPlate applications. By default, we are compiling the CSS
-  | file for the application as well as bundling up all the JavaScript files.
-  |
- */
-
+require('dotenv').config()
 const theme = process.env.WP_DEFAULT_THEME;
 
 mix.setResourceRoot('../');
 mix.setPublicPath(`public/themes/${theme}/public`);
 
-mix.js('resources/scripts/app.js', 'app.js');
-mix.postCss('resources/styles/app.css', 'app.css', [
-  //
-]);
+mix
+  .js(`public/themes/${theme}/app/js/app.js`, 'bundle.js')
+  .eslint({
+    fix: true,
+    extensions: ['js']
+  })
+  .sass(`public/themes/${theme}/app/scss/style.scss`, 'app.css')
+  .sourceMaps()
+  .stylelint()
