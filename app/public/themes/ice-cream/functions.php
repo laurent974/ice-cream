963ac36393a8fd631d3aa155c33aaddebb9917f1
Laurent <lyenpon974@gmail.com>
2022-05-27 01:28:01 +0200

    Add - Blocks gutenberg

--- app/public/themes/ice-cream/functions.php
+++ app/public/themes/ice-cream/functions.php
@@ -1,4 +1,31 @@
 <?php
+  /*
+   * Initalize Timber.
+  */
+
+  $timber = new \Timber\Timber();
+  // \Timber\Timber::$autoescape = true;
+
+  function allowed_blocks( $allowed_block_types, $post ) {
+    // Test du type de publication (facultatif)
+    if ( $post->post_type !== 'post' ) {
+      return $allowed_block_types;
+    }
+
+    // Ici on n'autorise que le paragraphe et l'image
+    return [ 'core/paragraph', 'core/image' ];
+  }
+
+  add_filter('timber_context', 'bt_timber_add_to_context');
+  function bt_timber_add_to_context ($context) {
+      // add current page content
+      $context['page'] = Timber::get_post();
+
+      return $context;
+  }
+
+  add_filter( 'allowed_block_types', 'allowed_blocks', 10, 2 );
+
   add_action('after_setup_theme', function () {
     add_theme_support('post-thumbnails');
     add_theme_support('title-tag');
--- app/public/themes/ice-cream/index.php
+++ app/public/themes/ice-cream/index.php
@@ -1,20 +1,6 @@
-<?php get_header(); ?>
+<?php
 
-<main role="main">
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <article>
-      <header>
-        <h1><?php the_title(); ?></h1>
-      </header>
-
-      <?php the_content(); ?>
-    </article>
-  <?php endwhile;
-    else : ?>
-  <article>
-    <p>Nothing to see.</p>
-  </article>
-  <?php endif; ?>
-</main>
-
-<?php get_footer(); ?>
+  $context = \Timber\Timber::context();
+  $context['menu']  = new Timber\Menu();
+  $context['page']  = \Timber\Timber::get_post();
+  \Timber\Timber::render('pages/index.twig', $context);
