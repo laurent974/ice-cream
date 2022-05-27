c04bcd42e3c8854fda37a35a4403631d9391edd1
Laurent <lyenpon974@gmail.com>
2022-05-27 11:04:42 +0200

    Update - Functions

--- app/public/themes/ice-cream/functions.php
+++ app/public/themes/ice-cream/functions.php
@@ -4,28 +4,8 @@
   */
 
   $timber = new \Timber\Timber();
-  // \Timber\Timber::$autoescape = true;
 
-  function allowed_blocks( $allowed_block_types, $post ) {
-    // Test du type de publication (facultatif)
-    if ( $post->post_type !== 'post' ) {
-      return $allowed_block_types;
-    }
-
-    // Ici on n'autorise que le paragraphe et l'image
-    return [ 'core/paragraph', 'core/image' ];
-  }
-
-  add_filter('timber_context', 'bt_timber_add_to_context');
-  function bt_timber_add_to_context ($context) {
-      // add current page content
-      $context['page'] = Timber::get_post();
-
-      return $context;
-  }
-
-  add_filter( 'allowed_block_types', 'allowed_blocks', 10, 2 );
-
+  // chai pas trop
   add_action('after_setup_theme', function () {
     add_theme_support('post-thumbnails');
     add_theme_support('title-tag');
@@ -34,3 +14,72 @@
       'navigation' => __('Navigation'),
     ]);
   });
+
+  // Desactive les commentaires
+  add_action('admin_init', function () {
+    // Redirect any user trying to access comments page
+    global $pagenow;
+
+    if ($pagenow === 'edit-comments.php') {
+      wp_redirect(admin_url());
+      exit;
+    }
+
+    // Remove comments metabox from dashboard
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+
+    // Disable support for comments and trackbacks in post types
+    foreach (get_post_types() as $post_type) {
+      if (post_type_supports($post_type, 'comments')) {
+        remove_post_type_support($post_type, 'comments');
+        remove_post_type_support($post_type, 'trackbacks');
+      }
+    }
+  });
+
+  // Close comments on the front-end
+  add_filter('comments_open', '__return_false', 20, 2);
+  add_filter('pings_open', '__return_false', 20, 2);
+
+  // Hide existing comments
+  add_filter('comments_array', '__return_empty_array', 10, 2);
+
+  // Remove comments page in menu
+  add_action('admin_menu', function () {
+    remove_menu_page('edit-comments.php');
+  });
+
+  // Remove comments links from admin bar
+  add_action('init', function () {
+    if (is_admin_bar_showing()) {
+      remove_action('admin_bar_menu', 'wp_admin_bar_comments_menu', 60);
+    }
+  });
+
+  // Function to change "posts" to "news" in the admin side menu
+function change_post_menu_label() {
+  global $menu;
+  global $submenu;
+  $menu[5][0] = 'Produits';
+  $submenu['edit.php'][5][0] = 'Produits';
+  $submenu['edit.php'][10][0] = 'Ajouter un produit';
+  $submenu['edit.php'][16][0] = 'Tags';
+  echo '';
+}
+add_action( 'admin_menu', 'change_post_menu_label' );
+// Function to change post object labels to "news"
+function change_post_object_label() {
+  global $wp_post_types;
+  $labels = &$wp_post_types['post']->labels;
+  $labels->name = 'Produits';
+  $labels->singular_name = 'Produit';
+  $labels->add_new = 'Ajouter un produit';
+  $labels->add_new_item = 'Ajouter un produit';
+  $labels->edit_item = 'Editer un produit';
+  $labels->new_item = 'Produit';
+  $labels->view_item = 'Voir le produit';
+  $labels->search_items = 'Rechercher un produit';
+  $labels->not_found = 'Aucun produit trouvé';
+  $labels->not_found_in_trash = 'Aucun produit dans la corbeille';
+}
+add_action( 'init', 'change_post_object_label' );
