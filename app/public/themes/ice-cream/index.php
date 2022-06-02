b5a4c21ed2e8d668c409d8a5f299dcda46cd787b
Laurent <lyenpon974@gmail.com>
2022-06-02 22:10:04 +0200

    Add - Menu footer

--- app/public/themes/ice-cream/index.php
+++ app/public/themes/ice-cream/index.php
@@ -1,6 +1,7 @@
 <?php
 
   $context = \Timber\Timber::context();
-  $context['menu']  = new Timber\Menu();
+  $context['menu']  = new Timber\Menu('Navigation principale');
+  $context['menu_footer']  = new Timber\Menu('Navigation footer');
   $context['page']  = \Timber\Timber::get_post();
   \Timber\Timber::render('pages/index.twig', $context);
